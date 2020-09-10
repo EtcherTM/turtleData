e13e539bae2c7c3eb1906727e2832a647bd841d7
Sebastien <jamesjwj128@gmail.com>
2020-09-10 22:25:01 +0100

    EditViewController populates but is not functional

--- turtleAppLayout/Controllers/EditViewController.swift
+++ turtleAppLayout/Controllers/EditViewController.swift
@@ -125,7 +125,9 @@ class EditViewController: UIViewController, UITextViewDelegate, UITextFieldDeleg
 
         }
         nestButton.setTitle(data!.nest ? data?.nestType : "", for: .normal)
+        disturbedButton.setTitle(data!.disturbed ? data?.disturbedOrRelocated : "", for: .normal)
         turtleButton.setTitle(data!.turtle ? data?.turtleType : "", for: .normal)
+        hatchingButton.setTitle(data!.hatching ? data?.hatchingType : "", for: .normal)
         commentsTextView.text = data!.comments
     }
 
