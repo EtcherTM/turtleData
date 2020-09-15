6594e329b6e2fdb855d1a28dec5a53664ecfec20
Sebastien <jamesjwj128@gmail.com>
2020-09-15 22:21:49 +0100

    Working on EditViewController.  Some problems remain.  Hatching information not saving, no H in identifier

--- turtleAppLayout/Controllers/EditViewController.swift
+++ turtleAppLayout/Controllers/EditViewController.swift
@@ -18,6 +18,9 @@ class EditViewController: UIViewController, UITextViewDelegate, UITextFieldDeleg
     
     var data: Observation?
     
+    var temp = Observation()
+    
+    
     
     let realm = try! Realm()
     let db = Firestore.firestore()
@@ -55,12 +58,14 @@ class EditViewController: UIViewController, UITextViewDelegate, UITextFieldDeleg
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if data?.id != nil {
+        if data != nil {
             title = "Edit \(data!.id)"
+            print("Data is not nil!!!")
             fillTextFields()
         }
         else {
             //            put an alert here?
+            print("eoirhoerihwoeirho")
         }
         
         
@@ -92,34 +97,70 @@ class EditViewController: UIViewController, UITextViewDelegate, UITextFieldDeleg
     }
     
     func fillTextFields () {
-        zoneButton.setTitle(data!.zoneLocation, for: .normal)
-        var propertyDesc = ""
-        //        if data!.property != nil {
-        //            let index = Int(String(data!.property.dropFirst().dropFirst()))! - 1 // Checking for nil but still fails
-        //            switch data?.property.first {
-        //            case "A":
-        //                propertyDesc = K.propertiesInA[index].1
-        //            case "B":
-        //            propertyDesc = K.propertiesInB[index].1
-        //            case "C":
-        //                propertyDesc = K.propertiesInC[index].1
-        //            case "D":
-        //                propertyDesc = K.propertiesInD[index].1
-        //            case "E":
-        //                propertyDesc = K.propertiesInE[index].1
-        //            case "F":
-        //                propertyDesc = K.propertiesInF[index].1
-        //            default:
-        //                propertyDesc = "No property/lot selected"
-        //            }
-        //        }
-        //        propertyButton.setTitle(propertyDesc, for: .normal)
+        temp.id = data!.id
+        temp.date = data!.date
         
-        if data!.lat != 0.0 || data!.lon != 0.0 {
+        temp.zoneLocation = data!.zoneLocation
+        temp.property = data!.property
+        
+        temp.lat = data!.lat
+        temp.lon = data!.lon
+        temp.accuracy = data!.accuracy
+        
+        temp.nest = data!.nest
+        temp.turtle = data!.turtle
+        temp.existingNestDisturbed = data!.existingNestDisturbed
+        
+        temp.nestType = data!.nestType
+        temp.turtleType = data!.turtleType
+        temp.existingNestDisturbedType = data!.existingNestDisturbedType
+        
+        temp.hatching = data!.hatching
+        
+        temp.numSuccess = data!.numSuccess
+        temp.numStranded = data!.numStranded
+        temp.numDead = data!.numDead
+        
+        temp.image1 = data!.image1
+        temp.image2 = data!.image2
+        temp.image3 = data!.image3
+        temp.image4 = data!.image4
+        temp.image5 = data!.image5
+        
+        temp.comments = data!.comments
+        
+        zoneButton.setTitle(data!.zoneLocation, for: .normal)
+        
+        
+        var propertyDesc = ""
+        
+        if var index = Int(String(data!.property.dropFirst().dropFirst())) {
+            index -= 1
+            switch data?.property.first {
+            case "A":
+                propertyDesc = K.propertiesInA[index].1
+            case "B":
+                propertyDesc = K.propertiesInB[index].1
+            case "C":
+                propertyDesc = K.propertiesInC[index].1
+            case "D":
+                propertyDesc = K.propertiesInD[index].1
+            case "E":
+                propertyDesc = K.propertiesInE[index].1
+            case "F":
+                propertyDesc = K.propertiesInF[index].1
+            default:
+                propertyDesc = "No property/lot selected"
+            }
+        }
+        propertyButton.setTitle(propertyDesc, for: .normal)
+        
+        
+        if data!.lat != 0.0 && data!.lon != 0.0 {
             let latAsStr = String(format: "%.2f", data!.lat)
             let lonAsStr = String(format: "%.2f", data!.lon)
-            //        let accAsStr = String(format: "%.1f", location.horizontalAccuracy)
-            locationButton.setTitle("\(latAsStr), \(lonAsStr) ± ____ m", for: .normal)
+            let accAsStr = String(format: "%.1f", data!.accuracy)
+            locationButton.setTitle("\(latAsStr), \(lonAsStr) ± \(accAsStr) m", for: .normal)
         } else {
             locationButton.setTitle("", for: .normal)
             
@@ -127,7 +168,7 @@ class EditViewController: UIViewController, UITextViewDelegate, UITextFieldDeleg
         nestButton.setTitle(data!.nest ? data?.nestType : "", for: .normal)
         disturbedButton.setTitle(data!.existingNestDisturbed ? data?.existingNestDisturbedType : "", for: .normal)
         turtleButton.setTitle(data!.turtle ? data?.turtleType : "", for: .normal)
-//        hatchingButton.setTitle(data!.hatching ? data?.hatchingType : "", for: .normal)
+        //        hatchingButton.setTitle(data!.hatching ? data?.hatchingType : "", for: .normal)
         commentsTextView.text = data!.comments
         
         //      Displaying images:
@@ -139,7 +180,7 @@ class EditViewController: UIViewController, UITextViewDelegate, UITextFieldDeleg
                 if var documentsPathURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
                     
                     documentsPathURL.appendPathComponent("\(images[image]).jpg")
-            
+                    
                     
                     print(documentsPathURL)
                     let imageToLoad = UIImage(contentsOfFile: documentsPathURL.path) ?? UIImage()
@@ -158,8 +199,9 @@ class EditViewController: UIViewController, UITextViewDelegate, UITextFieldDeleg
                         photoImageView5.image = imageToLoad
                     default:
                         print("Error loading images")
-                        
                     }
+                    
+                    
                 }
             }
         }
@@ -214,18 +256,19 @@ class EditViewController: UIViewController, UITextViewDelegate, UITextFieldDeleg
     
     @IBAction func zoneButtonPressed(_ sender: UIButton) {
         print("zone")
-        var zoneTemp = self.data!.zoneLocation
-        var propertyTemp = self.data!.property
+        print(temp)
+        
+        
         let alert = UIAlertController(title: "SELECT A ZONE", message: "", preferredStyle: .alert)
         
         for zone in K.zones {
             let action = UIAlertAction(title: zone, style: .default) { (_) in
-                if zoneTemp != zone {
-                    propertyTemp = ""
+                if self.temp.zoneLocation != zone {
+                    self.temp.property = ""
                     self.propertyButton.setTitle("", for: .normal)
                 }
                 sender.setTitle(zone, for: .normal)
-                zoneTemp = zone
+                self.temp.zoneLocation = zone
                 
             }
             alert.addAction(action)
@@ -233,8 +276,8 @@ class EditViewController: UIViewController, UITextViewDelegate, UITextFieldDeleg
         
         alert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: { (_) in
             sender.setTitle("", for: .normal)
-            zoneTemp = ""
-            propertyTemp = ""
+            self.temp.zoneLocation = ""
+            self.temp.property = ""
             self.propertyButton.setTitle("", for: .normal)
         }))
         
@@ -242,16 +285,58 @@ class EditViewController: UIViewController, UITextViewDelegate, UITextFieldDeleg
         alert.view.tintColor = UIColor.black
         
         
-        
     }
     @IBAction func propertyButtonPressed(_ sender: UIButton) {
         print("property")
+        
+        var propertyList: Array<(String, String)>?
+        switch temp.zoneLocation {
+        case "A":
+            propertyList = K.propertiesInA
+        case "B":
+            propertyList = K.propertiesInB
+        case "C":
+            propertyList = K.propertiesInC
+        case "D":
+            propertyList = K.propertiesInD
+        case "E":
+            propertyList = K.propertiesInE
+        case "F":
+            propertyList = K.propertiesInF
+        default:
+            let alert = UIAlertController(title: "SELECT ZONE FIRST", message: "", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true)
+        }
+        
+        
+        if let propertyList = propertyList {
+            let alert = UIAlertController(title: "SELECT A PROPERTY FROM ZONE \(temp.zoneLocation)", message: "", preferredStyle: .alert)
+            
+            for property in propertyList {
+                alert.addAction(UIAlertAction(title: "\(property.0) : \(property.1)", style: .default, handler: { (_) in
+                    self.temp.property = property.0
+                    sender.setTitle("\(property.0) : \(property.1)", for: .normal)
+                }))
+            }
+            
+            alert.addAction(UIAlertAction(title: "CLEAR SELECTION", style: .cancel, handler: { (_) in
+                sender.setTitle("Property/Lot?", for: .normal)
+                
+            }))
+            present(alert, animated: true)
+            alert.view.tintColor = UIColor.black
+            
+        }
         
     }
     
     @IBAction func locationButtonPressed(_ sender: UIButton) {
         print("location")
         
+        locationManager.requestLocation()
+        sender.setTitle("Getting: hold position . . .", for: .normal)
     }
     
     @IBAction func nestButtonPressed(_ sender: UIButton) {
@@ -296,6 +381,82 @@ class EditViewController: UIViewController, UITextViewDelegate, UITextFieldDeleg
     
     @IBAction func doneButtonPressed(_ sender: UIButton) {
         print("DONE!!!")
+        
+        let alert = UIAlertController(title: "SAVE THIS OBSERVATION AND CLEAR ALL FIELDS?", message: "", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (_) in
+            
+            self.temp.comments = self.commentsTextView.text ?? ""
+            
+            // creat id
+            var id = "\(self.temp.zoneLocation)-" != "" ? "\(self.temp.zoneLocation)-": "-"
+            
+            if self.temp.nest { id.append(self.temp.nestType == "nest" ? "N" : "F") }
+            if self.temp.existingNestDisturbed { id.append(self.temp.existingNestDisturbedType == "disturbed" ? "D" : "R") }
+            //            id.append(self.data.hatching ? "H" : "")
+            id.append(self.temp.turtle ? "T" : "")
+            
+            let dateFormatter = DateFormatter()
+            
+            dateFormatter.dateFormat = "-yyyyMMdd-HHmmss-"
+            
+            id.append(dateFormatter.string(from: Date()))
+            id.append(self.defaults.string(forKey: "userID") ?? "NOUSER")
+            self.temp.id = id
+            
+            
+            do {
+                try self.realm.write {
+                    self.realm.add(self.temp)
+                    self.realm.delete(self.data!)
+                }
+                
+                //                        //Reset all fields if successfully saved
+                //                        self.locationButton.setTitle("Get GPS Location", for: .normal)
+                //                        self.nestButton.setTitle("Nest?", for: .normal)
+                //                        self.existingNestButton.setTitle("Disturb/Reloc?", for: .normal)
+                //                        self.turtleButton.setTitle("Turtle?", for: .normal)
+                //                        self.hatchingButton.setTitle("Hatching", for: .normal)
+                //                        self.zoneButton.setTitle("Zone?", for: .normal)
+                //                        self.propertyButton.setTitle("Property/Lot?", for: .normal)
+                //                        self.commentsTextView.text = ""
+                //        //                self.photoButton.setTitle("", for: .normal)  What's wrong here?
+                //                        self.photoImage1.image = UIImage(systemName: "camera.fill")
+                //                        self.photoImage2.image = UIImage(systemName: "camera.fill")
+                //                        self.photoImage3.image = UIImage(systemName: "camera.fill")
+                //                        self.photoImage4.image = UIImage(systemName: "camera.fill")
+                //                        self.photoImage5.image = UIImage(systemName: "camera.fill")
+                
+                self.data = Observation()
+            } catch {
+                print("Error saving data, \(error) END")
+            }
+            
+        })) // Ends closure begun in line 279
+        alert.addAction(UIAlertAction(title: "NO", style: .cancel, handler: nil))
+        
+        present(alert, animated: true)
+        alert.view.tintColor = UIColor.black
+    }
+    @IBAction func deleteButtonPressed(_ sender: UIButton) {
+        print("deeeeeleting")
+        
+        let alert = UIAlertController(title: "Delete observation?", message: "", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: .cancel, handler: { (_) in
+            do {
+                try self.realm.write{
+                    self.realm.delete(self.data!)
+                }
+            } catch {
+                print("ERror deleting observation: \(error)")
+            }
+            
+            //Go back to main menu
+        }))
+        
+        alert.addAction(UIAlertAction(title: "no", style: .default, handler: nil))
+        
+        present(alert, animated: true)
     }
 }
 
@@ -305,12 +466,12 @@ class EditViewController: UIViewController, UITextViewDelegate, UITextFieldDeleg
 extension EditViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
-            //            data.lat = location.coordinate.latitude
-            //            data.lon = location.coordinate.longitude
-            //            let latAsStr = String(format: "%.2f", data.lat)
-            //            let lonAsStr = String(format: "%.2f", data.lon)
-            //            let accAsStr = String(format: "%.1f", location.horizontalAccuracy)
-            //            locationButton.setTitle("\(latAsStr), \(lonAsStr), ± \(accAsStr)m", for: .normal)
+            temp.lat = location.coordinate.latitude
+            temp.lon = location.coordinate.longitude
+            let latAsStr = String(format: "%.2f", temp.lat)
+            let lonAsStr = String(format: "%.2f", temp.lon)
+            let accAsStr = String(format: "%.1f", location.horizontalAccuracy)
+            locationButton.setTitle("\(latAsStr), \(lonAsStr), ± \(accAsStr)m", for: .normal)
         }
         
         
